e6fa36ba93211b2eb63d556544117ba6b621e3fb
Duwe Aynab <mfaynab88@gmail.com>
2022-07-08 15:32:22 -0400

    first commit!

---
+++ MovieQuiz/src/test/java/com/tts/MovieQuiz/MovieQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.MovieQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d48253acb7b29eae5ce2772aada8b69d09629789
Duwe Aynab <mfaynab88@gmail.com>
2022-07-09 14:37:15 -0400

    setting up api config to able to retrieve questions from categories.

--- MovieQuiz/moviequiz/src/Components/Settings.js
+++ MovieQuiz/moviequiz/src/Components/Settings.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState } from 'react';
 function Settings() {
+
+    const [options, setOptions] = useState(null);
+
+    const [questionCategory, setQuestionCategory] = useState("");
+
+    useEffect(() => {
+        const apiUrl = `https://opentdb.com/api_category.php`;
+
+        fetch(apiUrl)
+            .then((res) => res.json())
+            .then((response) => {
+              setOptions(response.trivia_categories);
+            });
+        }, [setOptions]);
+
+    const handleCategoryChange = event => {
+    setQuestionCategory(event.target.value)
+    }
+
     return(
-        <div></div>
+    <div>
+      </div>
+           <h2>Select Category:</h2>
+            <select value={questionCategory} onChange={handleCategoryChange}>
+              <options>All</options>
+                {options &&
+                     options.map((option) => (
+                     <option value={option.id} key={option.id}>
+                     {option.name}
+               </option>
+                ))}
+            </select>
+     </div>
+    </div>
     );
-
 }
-
 export default Settings;
